8508d890f8f681c9e399fa0bd79296be1f7bb93c
Bernardo Carvalho <bernardo.taveira.carvalho@gmail.com>
2022-05-06 18:40:09 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,93 @@
+var toastLiveExample = document.getElementById('liveToast')
+/*
+document.getElementById('liveToastBtn1').addEventListener('click', function () {
+
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+
+
+    toast.show()
+
+  })
+
+document.getElementById('liveToastBtn2').addEventListener('click', function () {
+
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+
+
+    toast.show()
+
+  });
+
+  document.getElementById('liveToastBtn3').addEventListener('click', function () {
+
+    alert("dsad");
+    //var toast = new bootstrap.Toast(toastLiveExample)
+
+
+
+    //toast.show()
+
+  })
+
+  document.getElementById('liveToastBtn4').addEventListener('click', function () {
+
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+
+
+    toast.show()
+
+  })
+
+  document.getElementById('liveToastBtn5').addEventListener('click', function () {
+
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+
+
+    toast.show()
+
+  })
+  document.getElementById('liveToastBtn6').addEventListener('click', function () {
+
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+
+
+    toast.show()
+
+  })
+
+  document.getElementById('liveToastBtn7').addEventListener('click', function () {
+
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+
+
+    toast.show()
+
+  })
+
+  document.getElementById('liveToastBtn8').addEventListener('click', function () {
+
+    var toast = new bootstrap.Toast(toastLiveExample)
+
+
+
+    toast.show()
+
+  })
+  */
+ function load(){
+   window.location.href = 'index.html#pagina-principal';
+   mySound = new Audio('https://www.youtube.com/watch?v=5XLOnYe7k9c');
+   mySound.play
+  }
+  
+
+  $(document).ready(function() {
+    jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 2000  // 2 seconds
+  });
+
